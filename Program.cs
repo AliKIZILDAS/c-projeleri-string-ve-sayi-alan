9d6d0daeff6357a6d497710488f09b9e1775ce94
Ali Kızıldaş <ali.kizildas@gmail.com>
2022-10-27 15:54:09 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime giriniz ve ardından çıkarmak istediğiniz karakterin indeksini giriniz(Aralarında virgül kullanınız.");
+            string s=Console.ReadLine();
+            string[] a=s.Split(",");
+            int b=Convert.ToInt32(a[1]);
+            string c=a[0].Remove(b,1);
+            Console.WriteLine(c);
+        }
+
+        
+    }
+}
